78bd727bbc732a27596fe51b2e35804e2463fe14
fenglisha <3113013893@qq.com>
2019-03-27 11:44:34 +0800

    删除一些代码

--- Testgit/src/com/git/one/TestGit1.java
+++ Testgit/src/com/git/one/TestGit1.java
@@ -2,8 +2,7 @@ package com.git.one;
 
 public class TestGit1 {
 public static void main(String[] args) {
-	System.out.println("第一个git提交");
-	System.out.println("第二次提交git");
+//	System.out.println("第一个git提交");
 	System.out.println("第三次提交git");
 	System.out.println("第四次提交git");
 	
