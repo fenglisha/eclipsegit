d7b6a066de2c591ae822f486c46aa6c61979b5a7
fenglisha <3113013893@qq.com>
2019-03-27 11:28:26 +0800

    第三次git提交

--- Testgit/src/com/git/one/TestGit1.java
+++ Testgit/src/com/git/one/TestGit1.java
@@ -2,7 +2,8 @@ package com.git.one;
 
 public class TestGit1 {
 public static void main(String[] args) {
+	System.out.println("第一个git提交");
 	System.out.println("第二次提交git");
-	System.out.println("第三次git提交");
+	System.out.println("第三次提交git");
 }
 }
